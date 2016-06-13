5f2c15e8b2785ecccbcb1ce1b8b8eee0ed47f3cf
JEFF HENRY <henry_jf@hotmail.com>
2016-06-13 15:00:59 -0400

    working on list columns

--- src/js/app.guitar/controllers/addguitar.controller.js
+++ src/js/app.guitar/controllers/addguitar.controller.js
@@ -1,9 +1,33 @@
+// function AddGuitarController(GuitarService, $state) {
+//
+//   let vm = this;
+//   vm.status = status;
+//   vm.addGuitar = addGuitar;
+//
+//   function addGuitar (guitar) {
+//
+//     console.log(guitar);
+//     GuitarService.add(guitar).then( (res) => {
+//
+//       $state.go('root.list')
+//     });
+//   }
+//
+// }
+//
+// AddGuitarController.$inject = ['GuitarService', '$state'];
+// export { AddGuitarController};
+
 function AddGuitarController(GuitarService, $state) {
 
   let vm = this;
   vm.status = status;
   vm.addGuitar = addGuitar;
-
+  vm.selected = selected;
+  // vm.wishList = wishList;
+  // vm.iOwn = iOwn;
+  console.log(selected);
+console.log(status);
   function addGuitar (guitar) {
 
     console.log(guitar);
