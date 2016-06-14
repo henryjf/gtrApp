a7eb2523ede968aaedd7844995e19951e81bfb7d
JEFF HENRY <henry_jf@hotmail.com>
2016-06-14 12:14:05 -0400

    login hide/show

--- src/js/app.core/run.js
+++ src/js/app.core/run.js
@@ -1,10 +1,18 @@
 function run ($rootScope, UserService) {
 
   $rootScope.$on('$stateChangeSuccess', (event, state) => {
-    UserService.checkAuth(state.name);
-  });
+    // UserService.checkAuth(state.name);
+  if (UserService.checkAuth(event, state) ) {
+    UserService.authChanged('authenticated');
+    $state.go('root.home');
+  } else {
+    UserService.authChanged('un-authenticated');
+    $state.go('root.login');
+  }
+});
+};
 
-}
+
 
 run.$inject = ['$rootScope', 'UserService'];
 export { run };
--- src/js/app.user/services/user.service.js
+++ src/js/app.user/services/user.service.js
@@ -1,9 +1,19 @@
-function UserService (Backand, $cookies, $state) {
+function UserService (Backand, $rootScope, $cookies, $state) {
 
   this.register  = register;
   this.login     = login;
   this.checkAuth = checkAuth;
   this.logout    = logout;
+  this.authChanged = authChanged;
+
+  function checkAuth () {
+    let token =$cookies.get('token');
+    return token ? true : false;
+  }
+
+  function authChanged (stat) {
+    $rootScope.$broadcast('authChanged', stat);
+  }
 
   function register (user) {
     return Backand.signup(
@@ -32,7 +42,7 @@ function UserService (Backand, $cookies, $state) {
     let user = $cookies.getObject('user');
     if (user) {
       console.log('user is logged in');
-    } else if (currentState === 'root.login' || currentState ==='root.register') {
+    } else if (currentState === 'root.login' || currentState ==='root.register' || currentState ==='root.home') {
       console.log('already on a login/register page');
     } else { $state.go('root.login');
     }
@@ -41,5 +51,5 @@ function UserService (Backand, $cookies, $state) {
 
 }
 
-UserService.$inject = ['Backand', '$cookies', '$state'];
+UserService.$inject = ['Backand', '$rootScope', '$cookies', '$state'];
 export { UserService };
